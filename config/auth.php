3f83f12cbf21ae39e71c3c6db94770e4a1769f8d
djawedch <120687949+djawedch@users.noreply.github.com>
2026-02-12 02:59:33 +0100

    refactor(auth): remove password reset flow (forgot/reset password)

--- config/auth.php
+++ config/auth.php
@@ -90,15 +90,6 @@ return [
     |
     */
 
-    'passwords' => [
-        'users' => [
-            'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
-        ],
-    ],
-
     /*
     |--------------------------------------------------------------------------
     | Password Confirmation Timeout
