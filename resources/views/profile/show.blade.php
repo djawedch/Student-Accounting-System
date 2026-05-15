c2a603571ccd142bdf4f2ea9bcfece9fa37d39b8
djawedch <120687949+djawedch@users.noreply.github.com>
2026-05-15 13:34:08 -0500

    style: update profile page to match StudiFi theme

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-gray-900 leading-tight">
             {{ __('My Profile') }}
         </h2>
     </x-slot>
@@ -8,8 +8,8 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    <h2 class="text-2xl font-semibold mb-4">Profile Information</h2>
+                <div class="p-6">
+                    <h2 class="text-2xl font-semibold mb-4 text-gray-900">Profile Information</h2>
 
                     <dl class="grid grid-cols-1 md:grid-cols-2 gap-4">
                         <div>
